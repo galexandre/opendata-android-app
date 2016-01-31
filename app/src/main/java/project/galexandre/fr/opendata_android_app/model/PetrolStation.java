56114470808e615123b8244250f511ce695a597e
Geoffrey ALEXANDRE <pygmyalex@gmail.com>
2016-01-31 20:51:15 +0000

    add a class (PetrolStation.java) and update some files

---
+++ app/src/main/java/project/galexandre/fr/opendata_android_app/model/PetrolStation.java
@@ -0,0 +1,20 @@
+package project.galexandre.fr.opendata_android_app.model;
+
+/**
+ * Created by galexandre on 31/01/16.
+ */
+public class PetrolStation {
+
+    private String id;
+    private String address;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
