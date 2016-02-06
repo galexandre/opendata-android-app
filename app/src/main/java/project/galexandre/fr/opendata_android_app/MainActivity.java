8abd0653e87b0fe78c474af530337a1883ef3e05
Geoffrey ALEXANDRE <pygmyalex@gmail.com>
2016-02-06 00:14:23 +0000

    remove .travis.yml and update mainActivity

--- app/src/main/java/project/galexandre/fr/opendata_android_app/MainActivity.java
+++ app/src/main/java/project/galexandre/fr/opendata_android_app/MainActivity.java
@@ -1,9 +1,17 @@
 package project.galexandre.fr.opendata_android_app;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import project.galexandre.fr.opendata_android_app.model.Station;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onStart(){
+        super.onStart();
+        Log.d("Debug","On start");
+        new HttpRequestTask();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -45,5 +60,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void,Void,Station>{
+
+        @Override
+        protected Station doInBackground(Void... params) {
+            try{
+                final String url="https://warm-headland-98018.herokuapp.com/stations";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Station station = restTemplate.getForObject(url,Station.class);
+                Log.d("Debug","Station:"+station);
+                return station;
+            }catch (Exception e){
+                Log.e("Error",e.getMessage(),e);
+            }
+            return null;
+
+        }
+
+        protected void onPostExecute(Station station){
+            TextView textViewIdValue = (TextView) findViewById(R.id.id_value);
+            TextView textViewAddressValue = (TextView) findViewById(R.id.address_value);
+            textViewIdValue.setText(station.getId());
+            textViewAddressValue.setText(station.getAddress()+" "+station.getCp()+" "+station.getCity());
+        }
+
     }
 }
